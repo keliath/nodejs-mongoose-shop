3d35b58056916c44938ca3f5ab3cbc388aff9986
carlos altamirano <carlos0altamirano@gmail.com>
2021-08-13 14:15:27 -0500

    add async deleting product

--- controllers/admin.js
+++ controllers/admin.js
@@ -181,8 +181,8 @@ exports.postEditProduct = (req, res, next) => {
     });
 };
 
-exports.postDeleteProduct = (req, res, next) => {
-  const prodId = req.body.productId;
+exports.deleteProduct = (req, res, next) => {
+  const prodId = req.params.productId;
   Product.findById(prodId)
     .then((product) => {
       if (!product) {
@@ -192,12 +192,14 @@ exports.postDeleteProduct = (req, res, next) => {
       return Product.deleteOne({ _id: prodId, userId: req.user._id });
     })
     .then((result) => {
-      res.redirect("/admin/product-list");
+      // res.redirect("/admin/product-list");
+      res.status(200).json({ message: "Success!" });
     })
     .catch((err) => {
-      const error = new Error(err);
-      error.httpStatusCode = 500;
-      return next(error);
+      // const error = new Error(err);
+      // error.httpStatusCode = 500;
+      // return next(error);
+      res.status(500).json({message: "Deleting product failed!"});
     });
 };
 
